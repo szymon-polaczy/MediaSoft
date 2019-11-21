b76df59c027c0378175de81678ad7a4594f87f34
szymon-polaczy <polaczyszymon@gmail.com>
2019-11-21 20:45:31 +0100

    I added some basic links and most of the needed webpages

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default props => (
+    <header>
+        <h1>{props.headerText}</h1>
+        <img src="https://mediasoft.com.pl/jpg/media-logo.png" alt="MediaSoft Logo"/>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/websites/">Strony WWW</Link></li>
+            <li><Link to="/hosting/">Hosting & Serwery</Link></li>
+            <li><Link to="/domains/">Domeny</Link></li>
+            <li><Link to="/google/">Linki Sponsorowane</Link></li>
+            <li><Link to="/contact/">Kontakt</Link></li>
+        </ul>
+    </header>
+)
--- src/pages/google.js
+++ src/pages/google.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from "../components/header"
 
-export default () => (
+export default() => (
     <div>
         <Header headerText="MediaSoft"/>
         <p>This is MediaSoft Webpage</p>
