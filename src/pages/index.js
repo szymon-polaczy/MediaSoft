3851f95c3e668200e518fb36eb806210f3941434
szymon-polaczy <polaczyszymon@gmail.com>
2019-11-24 00:07:26 +0100

    I added ContactUs Section everywhere and some more content into the Websites page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import MainLayout from "../components/MainLayout"
+import ContactUs from "../components/ContactUs"
 
 export default () => (
     <MainLayout>
         <p>This is MediaSoft Webpage</p>
+        <ContactUs/>
     </MainLayout>
 )
