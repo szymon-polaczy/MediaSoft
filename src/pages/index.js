24a1c8696c224286b1e99e9b8fa2441b49053060
szymon-polaczy <polaczyszymon@gmail.com>
2019-11-24 14:59:14 +0100

    I added more content to the Index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,42 @@
 import React from "react"
 import MainLayout from "../components/MainLayout"
 import ContactUs from "../components/ContactUs"
+import SellCard from "../components/SellCard"
 
 export default () => (
     <MainLayout>
-        <p>This is MediaSoft Webpage</p>
-        <ContactUs/>
+        <main>
+            <section className="welcome-section flex">
+                <div className="wrapper">
+                    <h1>Profesjonalna Obsługa Stron Internetowych</h1>
+                </div>
+            </section>
+            <section className="pad-it">
+                <div className="wrapper">
+                    <h1>Nasza oferta</h1>
+                    <small>Zapraszamy do zapoznania się z naszą ofertą</small>
+                    <div className="flex">
+                        <SellCard title_type="" title_name="Strony WWW" price="Od 300zł"/>
+                        <SellCard title_type="" title_name="Hosting" price="Od 109zł"/>
+                        <SellCard title_type="" title_name="Domeny" price="Od 52zł"/>
+                        <SellCard title_type="" title_name="Linki Sponsorowane" price="Od ~50zł"/>
+                    </div>
+                </div>
+            </section>
+            <ContactUs/>
+            <section className="pad-it">
+                <div className="wrapper">
+                    <h1>MediaSoft</h1>
+                    <small>Kilka słów o naszym zespole</small>
+                    <p className="higher-text">Od 2003 roku zajmujemy się kompleksową obsługą firm pragnących zaistnieć w Internecie 
+                        oraz zwiększyć efektywność swoich stron internetowych. Przez ten czas wykonaliśmy 
+                        blisko 300 stron internetowych, zarejestrowaliśmy w katalogach i wyszukiwarkach 
+                        oraz wypozycjonowaliśmy do TOP10 w Google.pl jeszcze więcej stron. Zwiększyliśmy 
+                        odwiedzalność stron i podnieśliśmy im PageRank. W naszej ofercie znajduje się także 
+                        stabilny hosting - serwery wirtualne oraz tanie domeny. Zapraszamy do zapoznania 
+                        się z naszą ofertą oraz odwiedzenia naszego portfolio stron WWW.</p>
+                </div>
+            </section>
+        </main>
     </MainLayout>
 )
