d4791b9c48c4b987803a515faf05cae18a32ccf5
szymon-polaczy <polaczyszymon@gmail.com>
2019-12-01 12:08:09 +0100

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => (
+    <header className="main-header">
+        <div className="flex justify-between wrapper">
+            <img src="https://mediasoft.com.pl/jpg/media-logo.png" alt="MediaSoft Logo"/>
+        </div>        
+    </header>
+)
