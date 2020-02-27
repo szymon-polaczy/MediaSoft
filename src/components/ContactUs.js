c263ff5f05625dee4a07a3b2aa4550862baecebc
szymon-polaczy <polaczyszymon@gmail.com>
2020-02-27 17:28:49 +0100

    r.1.1
    Zmieniłem styl stopki trochę, i styl sekcji do kontakowania się a redux sam się pojawił

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,25 +1,54 @@
 import React from "react"
+import styled from "styled-components"
+
+const ContactBar = styled.section`
+    padding: 5vh 0;
+    background: var(--theme-color);
+    color: #fff;
+    box-shadow: 0 0 3vh #bbb;
+    margin: 2.5rem 0;
+
+    .wrapper {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-column-gap: 4rem;
+    }
+
+    header i {
+        font-size: 2.5rem;
+        margin-right: 1.85rem;
+    }
+
+    h3 {
+        font-size: 1.65rem;
+    }
+
+    .phone {
+        font-size: 2rem;
+    }
+
+    .label {
+        font-size: .85rem;
+        display: block;
+        padding-bottom: .35rem;
+    }
+`
 
 export default() => (
-    <section className="contact-us-section">
+    <ContactBar className="contact-us-section">
         <div className="wrapper">
-            <h1>Chcesz się z nami skontaktować?</h1>
-            <div className="contain-contacts flex justify-around">
-                <div>
-                    <a href="mailto:kontakt@mediasoft.pl" className="contact-link flex justify-around">
-                        <i className="fas fa-envelope"></i>
-                        <p>kontakt@mediasoft.pl</p>
-                    </a>
+            <header className="flex flex-row">
+                <i className="fas fa-phone-alt"></i>
+                <div className="flex flex-column align-start">
+                    <p className="label">Masz pytania? Chcesz porozmawiać o ofercie?</p>
+                    <h3>Skontaktuj się z nami!</h3>
                 </div>
-                <div>
-                    <a href="tel:501-442-493" className="contact-link flex justify-around">
-                        <i className="fas fa-phone-alt"></i>
-                        <p>501 442 493</p>
-                    </a>
-                </div>
+            </header>
+            <p className="flex phone">(+ 48) 501 442 493</p>
+            <div className="flex flex-column align-start">
+                <p className="label">Adres e-mail:</p>
+                <h3>kontakt@mediasoft.com.pl</h3>
             </div>
         </div>
-    </section>
+    </ContactBar>
 )
-
-
