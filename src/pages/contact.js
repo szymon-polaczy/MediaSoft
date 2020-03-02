694f3a288cf2fcb921eb7518e468bfbd97832513
szymon-polaczy <polaczyszymon@gmail.com>
2020-03-02 18:18:04 +0100

    r.1.3.7
    Pracuje nad przerobieniem większości stylów tego projektu na styled-components, co myślę że powinno pomóc w oddzielaniu gdzie co się znajduje oraz w przyszłym rozwijaniu i ponowynym używaniu projektu

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,31 @@ import MainLayout from "../components/MainLayout"
 import GoogleMapsEmbed from "../components/GoogleMapsEmbed"
 import { useStaticQuery, graphql } from "gatsby"
 import WelcomeSection from "../components/WelcomeSection"
+import styled from "styled-components"
+
+const WelcomeText = styled.p`
+    text-align: center;
+    margin-top: 2.5rem;
+    line-height: 150%;
+    padding: 0 8.25rem;
+    color: #4f4f4f;
+
+    @media screen and (max-width: 1350px) {
+        padding: 0 5rem;
+    }
+
+    @media screen and (max-width: 950px) {
+        padding: 0 2rem;
+    }
+
+    @media screen and (max-width: 600px) {
+        padding: 0 1rem;
+    }
+
+    @media screen and (max-width: 350px) {
+        padding: 0 .4rem;
+    }
+`;
 
 export default () => {
     const data = useStaticQuery(graphql`
@@ -24,13 +49,16 @@ export default () => {
                 <section className="pad-it">
                     <div className="wrapper">
                         <h1>MediaSoft</h1>
-                        <p className="higher-text">Od 2003 roku zajmujemy się kompleksową obsługą firm pragnących zaistnieć w Internecie 
+                        <small>Poznajmy się bliżej</small>
+                        <WelcomeText>
+                            Od 2003 roku zajmujemy się kompleksową obsługą firm pragnących zaistnieć w Internecie 
                             oraz zwiększyć efektywność swoich stron internetowych. Przez ten czas wykonaliśmy 
                             blisko 300 stron internetowych, zarejestrowaliśmy w katalogach i wyszukiwarkach 
                             oraz wypozycjonowaliśmy do TOP10 w Google.pl jeszcze więcej stron. Zwiększyliśmy 
                             odwiedzalność stron i podnieśliśmy im PageRank. W naszej ofercie znajduje się także 
                             stabilny hosting - serwery wirtualne oraz tanie domeny. Zapraszamy do zapoznania 
-                            się z naszą ofertą oraz odwiedzenia naszego portfolio stron WWW.</p>
+                            się z naszą ofertą oraz odwiedzenia naszego portfolio stron WWW.
+                        </WelcomeText>
                     </div>
                 </section>
                 <div className="wrapper pad-it flex flex-column">
