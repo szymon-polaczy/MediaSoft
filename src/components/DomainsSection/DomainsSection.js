5efe4e4d7e69a6e3e5c7f970a3d2666eb05d65fd
szymon-polaczy <polaczyszymon@gmail.com>
2020-04-02 19:21:58 +0200

    r.0.9
    Domains-Section ma swoj pierwszy draft i wygląda całkiem ciekawie - prosto ale ciekawie

--- src/components/DomainsSection/DomainsSection.js
+++ src/components/DomainsSection/DomainsSection.js
@@ -1,7 +1,138 @@
 import React from 'react'
+import styled from 'styled-components'
+import Hostings from '../../images/undraw_ask_me_anything_k8o0.svg'
+
+const BigInfo = styled.h2`
+    font-size: 2.5rem;
+    letter-spacing: .05rem;
+    line-height: 120%;
+    margin: 1rem 0 1.5rem 0;
+    text-align: right;
+
+    &::after {
+        content: '';
+        display: block;
+        width: 20rem;
+        height: 2px;
+        margin-left: auto;
+        background-color: #666;
+        margin-top: .3rem;
+        background: #45B649;
+    }
+`;
+
+const Try = styled.section`
+    margin-top: 5rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+
+    section {
+        padding: 0 5rem;
+    }
+
+    section:last-child {
+        box-sizing: border-box;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    section img {
+        width: 100%;
+        transform: translateX(-50px);
+    }
+
+    .domains-wrapper {
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        flex-flow: wrap;
+    }
+
+    article {
+        width: 11rem;
+        height: 11rem;
+        margin: 1.35rem;
+        box-shadow: 0 0 .4rem #e6e6e6;
+        text-align: center;
+        font-size: 1.3rem;
+        display: flex;
+        align-items: center;
+        flex-flow: column;
+        justify-content: center;
+        border-radius: .65rem;
+    }
+
+    article h4 {
+        color: #fd746c;
+        font-size: 1.8rem;
+        letter-spacing: .03rem;
+        padding-bottom: .65rem;
+        font-weight: bold;
+    }
+
+    article span {
+        display: block;
+        color: #3a7bd5;
+        font-size: 1.45rem;
+        letter-spacing: .02rem;
+        padding: .75rem 0 .45rem 0;
+        font-weight: bold;
+    }
+
+    article p:last-child {
+        font-size: 1rem;
+        font-style: italic;
+        margin-top: .4rem;
+    }
+`;
 
 export default () => {
     return (
-        <h1>Domain Section</h1>
+        <Try>
+            <section>
+                <BigInfo>Wybierz domenę dla swojej strony</BigInfo>
+                <div className="domains-wrapper">
+                    <article>
+                        <h4>.pl</h4>
+                        <p>Pierwszy rok <span>10.90zł</span></p>
+                        <p>Odnowienie 109zł</p>
+                    </article>
+                    <article>
+                        <h4>.com</h4>
+                        <p>Pierwszy rok <span>49.90zł</span></p>
+                        <p>Odnowienie 89zł</p>
+                    </article>
+                    <article>
+                        <h4>.org</h4>
+                        <p>Pierwszy rok <span>49.90zł</span></p>
+                        <p>Odnowienie 89zł</p>
+                    </article>
+                    <article>
+                        <h4>.net</h4>
+                        <p>Pierwszy rok <span>49.90zł</span></p>
+                        <p>Odnowienie 89zł</p>
+                    </article>
+                    <article>
+                        <h4>.biz</h4>
+                        <p>Pierwszy rok <span>49.90zł</span></p>
+                        <p>Odnowienie 89zł</p>
+                    </article>
+                    <article>
+                        <h4>.info</h4>
+                        <p>Pierwszy rok <span>49.90zł</span></p>
+                        <p>Odnowienie 89zł</p>
+                    </article>
+                    <article>
+                        <h4>.eu</h4>
+                        <p>Pierwszy rok <span>119zł</span></p>
+                        <p>Odnowienie 129zł</p>
+                    </article>
+                </div>
+            </section>
+            <section>
+                <img src={Hostings} alt="Hosting Logo" />
+            </section>
+        </Try>
     )
 }
--- src/components/HostingSection/HostingSection.js
+++ src/components/HostingSection/HostingSection.js
@@ -19,88 +19,6 @@ const BigInfo = styled.h2`
     }
 `;
 
-const Holder = styled.article`
-    border-radius: .65rem;
-    overflow: hidden;
-    box-shadow: 0 0 .35rem #eee;
-
-    h3 {
-        font-size: 1.3rem;
-        line-height: 150%;
-        text-align: center;
-        background: #fff;
-    }
-
-    small {
-        text-align center;
-        width: 100%;
-        display: block;
-        position: relative;
-        z-index: -1;
-        box-shadow: 0 0 .35rem #ccc;
-        padding-bottom: 1rem;
-    }
-
-    h3 span {
-        display: inline-block;
-        padding-bottom: .25rem;
-    }
-
-    h3::before {
-        content: '';
-        display: block;
-        height: .65rem;
-        margin-bottom: .65rem;
-        width: 100%;
-        /*border-radius: .65rem .65rem 0 0;*/
-    }
-
-    .basic::before {
-        background: linear-gradient(to right, #FFAFBD, #ffc3a0);
-    }
-
-    .advanced::before {
-        background: linear-gradient(to right, #f4c4f3, #fc67fa);
-    }
-
-    .professional::before {
-        background: linear-gradient(to right, #ff7e5f, #feb47b);
-    }
-
-    .professional-plus::before {
-        background: linear-gradient(to right, #4ecdc4, #7bc6cc);
-    }
-
-    ul {
-        padding-bottom: 1.5rem;
-    }
-
-    ul li {
-        font-size: .9rem;
-        line-height: 110%;
-        padding: .5rem 3rem;
-        border-bottom: 1px solid #ccc;
-    }
-
-    ul li:last-child {
-        border: none;
-    }
-
-    section h4 {
-        text-align: center;
-        letter-spacing: .05rem;
-        font-size: 1.1rem;
-        margin-bottom: .65rem;
-        padding: 1rem 0 .5rem 0;
-    }
-`;
-
-const Container = styled.div`
-    display: grid;
-    gap: 2rem 5rem;
-    grid-template-columns: repeat(3, 1fr);
-`;
-
 const Try = styled.section`
     margin-top: 5rem;
     display: grid;
@@ -163,83 +81,6 @@ const Try = styled.section`
 
 export default () => {
     return (
-        <>
-        {/*<section className="margin-top-medium">
-            <BigInfo>Nasz Hosting Stron Internetowych</BigInfo>
-            <Container className="wrapper">
-                <Holder>
-                    <h3 className="basic">Serwery Wirtualne <span>Basic</span></h3>
-                    <small>już od 109zł netto</small>
-                    <section>
-                        <h4>Co możemy Ci Zaoferować?</h4>
-                        <ul>
-                            <li>✓ Powierzchnia dyskowa: 1GB/2GB</li>
-                            <li>✓ Konto FTP</li>
-                            <li>✓ Jedno konto mailowe</li>
-                            <li>✓ Kopia Bezpieczeństwa Raz W Tygodniu</li>
-                            <li>✓ Powiadomienia Email</li>
-                        </ul>
-                    </section>
-                </Holder>
-
-                <Holder>
-                    <h3 className="advanced">Serwery Wirtualne <span>Advanced</span></h3>
-                    <small>już od 159zł netto</small>
-                    <section>
-                        <h4>Co możemy Ci Zaoferować?</h4>
-                        <ul>
-                            <li>✓ Powierzchnia dyskowa: 3GB/5GB</li>
-                            <li>✓ Konto FTP</li>
-                            <li>✓ Max 5 kont mailowe</li>
-                            <li>✓ Kopia Bezpieczeństwa Co drugi dzień</li>
-                            <li>✓ Oprogramowanie antywirusowe</li>
-                            <li>✓ Powiadomienia Email i SMS</li>
-                            <li>✓ Statystyki</li>
-                        </ul>
-                    </section>
-                </Holder>
-
-                <Holder>
-                    <h3 className="professional">Serwery Wirtualne <span>Professional</span></h3>
-                    <small>już od 339zł netto</small>
-                    <section>
-                        <h4>Co możemy Ci Zaoferować?</h4>
-                        <ul>
-                            <li>✓ Powierzchnia dyskowa: 10GB</li>
-                            <li>✓ Konto FTP</li>
-                            <li>✓ Nielimitowane konta mailowe</li>
-                            <li>✓ Kopia Bezpieczeństwa Codziennie</li>
-                            <li>✓ Oprogramowanie antywirusowe</li>
-                            <li>✓ Powiadomienia Email i SMS</li>
-                            <li>✓ Nielimitowana liczba domen</li>
-                            <li>✓ Nielimitowana ilość baz danych</li>
-                            <li>✓ Statystyki</li>
-                            <li>✓ Autoresponder</li>
-                        </ul>
-                    </section>
-                </Holder>
-
-                <Holder>
-                    <h3 className="professional-plus">Serwery Dedykowane <span>Professional+</span></h3>
-                    <small>Zadzwoń aby się dowiedzieć</small>
-                    <section>
-                        <h4>Co możemy Ci Zaoferować?</h4>
-                        <ul>
-                            <li>✓ Powierzchnia dyskowa: 10GB</li>
-                            <li>✓ Konto FTP</li>
-                            <li>✓ Nielimitowane konta mailowe</li>
-                            <li>✓ Kopia Bezpieczeństwa Codziennie</li>
-                            <li>✓ Oprogramowanie antywirusowe</li>
-                            <li>✓ Powiadomienia Email i SMS</li>
-                            <li>✓ Nielimitowana liczba domen</li>
-                            <li>✓ Nielimitowana ilość baz danych</li>
-                            <li>✓ Statystyki</li>
-                            <li>✓ Autoresponder</li>
-                        </ul>
-                    </section>
-                </Holder>
-            </Container>
-        </section>*/}
         <Try>
             <section>
                 <img src={Hostings} alt="Hosting Logo" />
@@ -270,6 +111,5 @@ export default () => {
                     We are one of the easiest website hosting platforms to use, and remain committed to providing our customers with one of the best hosting solutions on the market.</p>
             </section>
         </Try>
-        </>
     )
 }
