18c53f2c15094359b780b34510ab097daca47f95
szymon-polaczy <polaczyszymon@gmail.com>
2020-07-31 21:29:19 +0200

    I'm starting to work on privacy-policy page

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useStaticQuery, graphql } from "gatsby"
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser'
+import styled from 'styled-components'
+
+const PolicyHolder = styled.main`
+    strong {
+        font-weight: bold;
+    }
+
+    p {
+        margin: .65rem 0;
+    }
+`
+
+const PrivacyPolicy = () => {
+    const data = useStaticQuery(graphql`
+    {
+      datoCmsPrivacypolicy {
+        policy
+      }
+    }
+  `)
+
+    return (
+        <PolicyHolder>
+            {ReactHtmlParser(data.datoCmsPrivacypolicy.policy)}
+        </PolicyHolder>
+    )
+}
+
+export default PrivacyPolicy
