a6df43772f27cd68b1e8ab75690d01d165bf253c
szymon-polaczy <polaczyszymon@gmail.com>
2020-09-13 11:26:14 +0200

    Another One

--- src/components/ContactSection/Form.js
+++ src/components/ContactSection/Form.js
@@ -193,11 +193,13 @@ async function postData(url = '', data = {}) {
   // Default options are marked with *
   const response = await fetch(url, {
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'no-cors', // no-cors, *cors, same-origin
+    mode: 'cors', // no-cors, *cors, same-origin
     //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     //credentials: 'same-origin', // include, *same-origin, omit
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'POST'
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
     //redirect: 'follow', // manual, *follow, error
